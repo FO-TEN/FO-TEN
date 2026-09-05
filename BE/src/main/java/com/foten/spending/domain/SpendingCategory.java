e81f8308fe781a7914017c778a9b7c008dcb42c9
Minju Kang <brio0106@gmail.com>
2026-09-05 15:36:51 +0900

    [be]refactor: 소비 카테고리 목록을 도메인 상수로 분리
    서비스 안에 private 상수로 있어서 다른 곳에서 같은 목록이 필요하면
    복사해야 한다. 하나만 고쳤을 때 조용히 어긋난다.
    
    곧 챗봇 선택지에서 같은 목록을 쓴다.

---
+++ BE/src/main/java/com/foten/spending/domain/SpendingCategory.java
@@ -0,0 +1,10 @@
+package com.foten.spending.domain;
+
+import java.util.List;
+
+public final class SpendingCategory {
+    public static final List<String> ALL = List.of("식비", "교통", "통신", "쇼핑", "기타");
+
+    private SpendingCategory() {
+    }
+}
